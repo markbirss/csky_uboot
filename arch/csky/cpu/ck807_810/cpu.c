8e3d6c57e14c347a704c50d637b4796c9a7d62e9
zhangmf <minfeng_zhang@c-sky.com>
2017-11-09 11:03:33 +0800

    Add the cmd of reset
    Change-Id: I7b5e0a686a4d28b4cf9a374c069ee1127e817d36

--- arch/csky/cpu/ck807_810/cpu.c
+++ arch/csky/cpu/ck807_810/cpu.c
@@ -7,12 +7,18 @@
 #include <watchdog.h>
 #include <command.h>
 #include <netdev.h>
+#include <asm/arch/hardware.h>
+#include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int do_reset(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
 {
-	return 1;
+	u32 temp;
+	temp = readl(CHIP_CTRL_BASEADDR+0xa0000000+SOFTWARE_RESET_CONTROL);
+	temp = temp & 0x1;
+	writel(temp, CHIP_CTRL_BASEADDR+0xa0000000+SOFTWARE_RESET_CONTROL);
+	return 0;
 }
 
 void relocate_code(ulong start_addr_sp, gd_t *new_gd, ulong relocaaddr)
